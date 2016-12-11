cb1327d78c5e638a3db30a1e6db1b1daefda3e5b
U-Johan-7\Johan <johan_cattin@hotmail.com>
2016-12-11 18:12:59 +0100

    parsing in progress

--- SDA-TP10/src/MinimumSpanningTree.java
+++ SDA-TP10/src/MinimumSpanningTree.java
@@ -1,6 +1,28 @@
+import java.io.*;
+import java.util.LinkedHashSet;
+
 /**
  * Created by Johan on 11.12.2016.
  */
 public class MinimumSpanningTree {
-    
+    private static LinkedHashSet<City> citySet;
+
+    public static void main(String[] args) throws IOException{
+        FileWriter writer = new FileWriter("out/MST.out");
+        FileInputStream inStream = new FileInputStream("C:\\Users\\Johan\\Desktop\\mael\\SDA-TP10\\res\\sda_graph.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+        citySet = new LinkedHashSet<>();
+        String line;
+        String[] dists;
+        City current;
+        City next;
+        while((line = reader.readLine()) != null){
+            try {
+                City city = new City(line);
+            } catch (IllegalArgumentException e){
+                dists = line.split(" ");
+            }
+
+        }
+    }
 }
